099e3c174e9e9ae4c9cdc5ad5388cd4f0256d992
as-real-me <real.uiucse@gmail.com>
2020-07-20 08:45:59 +0600

    Basic Events - Onload, Onresize, Onclick

---
+++ Interactivity with JavaScript/Week 02/basic-events.js
@@ -0,0 +1,7 @@
+function msg(msg){
+    document.getElementById('show-output').innerHTML = msg + " event is happening now!";
+}
+
+function showDate(){
+    document.getElementById('date-demo').innerHTML = Date();
+}

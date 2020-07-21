02336d5e92c8d2788f6c3685be84853e3f3de2ac
as-real-me <real.uiucse@gmail.com>
2020-07-21 08:10:43 +0600

    Week 03 - Code With Me - Array

---
+++ Interactivity with JavaScript/Week 03/array.js
@@ -0,0 +1,11 @@
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+function loadFruits(){
+    document.getElementById('fruits').innerHTML = fruits;
+}
+
+function myFunc(){
+    var fruit = prompt("What is your favourite fruit?");
+    fruits[fruits.length] = fruit;
+    document.getElementById('fruits').innerHTML = fruits;
+}

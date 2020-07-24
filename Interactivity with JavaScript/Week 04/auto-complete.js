98bc0b1c0b0e902977f0e9cb46969abeb6a69d2d
as-real-me <real.uiucse@gmail.com>
2020-07-24 07:06:31 +0600

    Added comment

--- Interactivity with JavaScript/Week 04/auto-complete.js
+++ Interactivity with JavaScript/Week 04/auto-complete.js
@@ -1,9 +1,9 @@
 function billingFunction(){
     if(document.getElementById('same').checked){
-        document.getElementById('billingName').value = document.getElementById('shippingName').value;
-        document.getElementById('billingZip').value = document.getElementById('shippingZip').value;
+        document.getElementById('billingName').value = document.getElementById('shippingName').value; // Auto completing the Billing Name when same is checked
+        document.getElementById('billingZip').value = document.getElementById('shippingZip').value; // Auto completing the Billing Zip when same is checked
      }else{
-        document.getElementById('billingName').value = "";
-        document.getElementById('billingZip').value = "";
+        document.getElementById('billingName').value = ""; // Clearing the Billing Name when same is unchecked 
+        document.getElementById('billingZip').value = ""; // Clearing the Billing Zip when same is unchecked 
      }
 }
